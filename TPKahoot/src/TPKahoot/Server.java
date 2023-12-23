2260c70c824bb94a770348e70e45057aae5ebfd6
raydi <raydi@192.168.0.2>
2023-12-23 17:07:09 -0500

    1st modify xinye ge

--- TPKahoot/src/TPKahoot/Server.java
+++ TPKahoot/src/TPKahoot/Server.java
@@ -4,24 +4,34 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-public class Server implements Runnable{
+public class Server extends JFrame implements Runnable , Updatable{
 
 	private ArrayList<Thread> connectionThread;
 	private ArrayList<Connection> connections;
 	private int portNumber;
 	private ServerSocket serverSocket;
+	private boolean isRunning = false;
 	
 	public Server(int portNumber) {
 		connectionThread = new ArrayList<Thread>();
 		connections = new ArrayList<Connection>();
 		this.portNumber = portNumber;
+		
+		setTitle("sss");
+		setBounds(200,200,700,500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	public void run() {
+		if(isRunning) {
+			return;
+		}
+		isRunning = true;
 		portNumber = 5555;
-        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+       /* try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
             System.out.println("Server opened on port " + portNumber);
 
             this.serverSocket = serverSocket;
@@ -39,7 +49,7 @@ public class Server implements Runnable{
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 	
 	public void sendQuestion() {
@@ -67,5 +77,6 @@ public class Server implements Runnable{
 		}
 		JOptionPane.showMessageDialog(null, "Server was closed");
 	}
+	public boolean stillRunning() {return isRunning;}
 
 }
